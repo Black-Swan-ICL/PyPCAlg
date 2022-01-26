4dd5436142083da4a0fc3474bc2e92f1e8f30edc
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2022-01-26 13:19:42 +0000

    Readied infra to test PC adjacency on example 1

--- PC/test/test_pc_algorithm.py
+++ PC/test/test_pc_algorithm.py
@@ -4,6 +4,13 @@ import pytest
 from PC.pc_algorithm import run_pc_adjacency_phase, run_pc_orientation_phase, \
     run_pc_algorithm
 
+from PC.examples.graph_1 import generate_data as generate_data_example_1
+from PC.examples.graph_1 import get_graph_skeleton as skeleton_example_1
+from PC.examples.graph_1 import oracle_indep_test as \
+    oracle_indep_test_example_1
+from PC.examples.graph_1 import oracle_cond_indep_test as \
+    oracle_cond_indep_test_example_1
+
 from PC.examples.graph_4 import generate_data as generate_data_example_4
 from PC.examples.graph_4 import get_graph_skeleton as skeleton_example_4
 from PC.examples.graph_4 import oracle_indep_test as \
@@ -17,12 +24,20 @@ from PC.examples.graph_4 import oracle_cond_indep_test as \
     'expected_separation_sets',
     [
         (
-            generate_data_example_4(10),
-            oracle_indep_test_example_4(),
-            oracle_cond_indep_test_example_4(),
+            generate_data_example_1(10),
+            oracle_indep_test_example_1(),
+            oracle_cond_indep_test_example_1(),
             0.05,
-            skeleton_example_4(),
+            skeleton_example_1(),
             set()  # TODO change when ready
+        ),
+        (
+                generate_data_example_4(10),
+                oracle_indep_test_example_4(),
+                oracle_cond_indep_test_example_4(),
+                0.05,
+                skeleton_example_4(),
+                set()  # TODO change when ready
         ),
     ]
 )
