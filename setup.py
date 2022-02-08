5c979c77bf59383733cc566ac78c70212ccb8839
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2022-02-08 16:34:48 +0000

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / 'README.md').read_text()
+
+setup(
+    name='PyPCAlg',
+    version='1.0.0',
+    description='A Python implementation of the original PC algorithm.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Black-Swan-ICL/PyRKHSstats',
+    author='K. M-H',
+    author_email='kmh.pro@protonmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Financial and Insurance Industry',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.22.0',
+        'pandas',
+        'matplotlib',
+        'scikit-learn',
+        'pingouin'
+    ],
+    python_requires=">=3.9"
+)
